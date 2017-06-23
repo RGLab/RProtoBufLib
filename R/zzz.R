3455a5bf5711671d080922b46f7312bacde696da
mikejiang <wjiang2@fhcrc.org>
2017-06-23 10:35:55 -0700

    update

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,7 @@
 #' @examples 
 #' LdFlags()
 #' @export
+#' @return nothing.
 LdFlags <- function() {
   libpath <- paste0("lib", Sys.getenv("R_ARCH"), "/libprotobuf.a")
   cat(tools::file_path_as_absolute(base::system.file(libpath, package = "RProtoBufLib")))
