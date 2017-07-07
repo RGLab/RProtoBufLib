dc1f69472ae732829bde847ee9de3809bac104a9
Martin Morgan <mtmorgan@fhcrc.org>
2017-07-07 09:22:10 +0000

    Adding mfa, RProtoBufLib, Rhdf5lib, scoreInvHap
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RProtoBufLib@131032 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+#' linker flags
+#' Generate linker flags
+#' This function prints the absolute path to the protobuf library file that is shipped with the package.
+#' So that user package can use it in their Makevar file.
+#' @examples 
+#' LdFlags()
+#' @export
+#' @return nothing.
+LdFlags <- function() {
+  libpath <- paste0("lib", Sys.getenv("R_ARCH"), "/libprotobuf.a")
+  cat(tools::file_path_as_absolute(base::system.file(libpath, package = "RProtoBufLib")))
+}
